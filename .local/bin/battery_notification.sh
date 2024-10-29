ac4b007e3e166cf393e5ed6cebdd528990d29d4a
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2024-10-29 13:05:31 +0530

    Working battery alerts

--- .local/bin/battery_notification.sh
+++ .local/bin/battery_notification.sh
@@ -8,11 +8,11 @@ BATTERY_INFO=$(acpi -b)
 BATTERY_LEVEL=$(echo "$BATTERY_INFO" | grep -P -o '[0-9]+(?=%)')
 BATTERY_STATUS=$(echo "$BATTERY_INFO" | awk '{print $3}' | tr -d ',')
 
-echo "Battery level: ${BATTERY_LEVEL}%"
-echo "Battery status: ${BATTERY_STATUS}"
+echo "Battery Info: ${BATTERY_INFO}"
+
 
 # Check if the battery is discharging and the level is below the threshold
 if [[ "$BATTERY_STATUS" == "Discharging" && "$BATTERY_LEVEL" -le "$THRESHOLD" ]]; then
     # Send a notification using dunst
-    notify-send -u critical "Low Battery" "Battery level is ${BATTERY_LEVEL}%, ${BATTERY_INFO}"
+    notify-send -u critical "Low Battery" "(${BATTERY_LEVEL}%), ${BATTERY_INFO}"
 fi
