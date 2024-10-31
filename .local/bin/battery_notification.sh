e4c2a3303507064ee9a1e81655d798039dd831d2
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2024-10-31 15:47:02 +0530

    update: full battery check

--- .local/bin/battery_notification.sh
+++ .local/bin/battery_notification.sh
@@ -31,9 +31,13 @@ LAST_BATTERY_LEVEL=$(cat "$BAT_STATE_FILE" 2>/dev/null || echo "")
 echo "Battery Level: $BATTERY_LEVEL"
 echo "Battery Status: $BATTERY_STATUS"
 echo "Last Battery Level: $LAST_BATTERY_LEVEL"
-
-
 echo "$((LAST_BATTERY_LEVEL - BATTERY_LEVEL))"
+
+if [[ "$BATTERY_LEVEL" -gt "95" ]]; then
+    echo "Battery is full"
+    notify-send -a System -u low "Battery Full" "Battery is charged" -i battery-full
+fi
+
 if [[ "$BATTERY_STATUS" == "Discharging" && "$BATTERY_LEVEL" -le "$THRESHOLD" ]]; then
     if [ $((LAST_BATTERY_LEVEL - BATTERY_LEVEL)) -ge 5 ]; then
         notify-send -a System -u critical "Low Battery" "(${BATTERY_LEVEL}%), ${FORMATTED_TIME} remaining" -i battery-caution
